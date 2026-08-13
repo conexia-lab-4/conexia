8fedee00ab6eccffeb1a078d2767c3ec94620fec
guadalupehani <guadahani@icloud.com>
2026-08-13 11:10:58 -0300

    configurar plugin PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Conexia',
+        short_name: 'Conexia',
+        description: 'Conexia - Aplicación PWA',
+        theme_color: '#863bff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
+  ],
+});
