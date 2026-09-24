6bf2cc3ea0764d49b5ce38eadffee81634940488
MartiNogueira <monstercat387@gmail.com>
2026-09-24 20:12:41 -0300

    fix: quitar campo neighborhood del perfil del estudiante
    - Elimina neighborhood del schema, DTO, service y tests
    - Nueva migración remove_neighborhood que borra la columna de StudentProfile

---
+++ backend/prisma/migrations/20260924231114_remove_neighborhood/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `neighborhood` on the `StudentProfile` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "StudentProfile" DROP COLUMN "neighborhood";
